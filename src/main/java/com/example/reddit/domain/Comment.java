a4a8d30ad906b4eee1689cf0966a733b92d1f82d
omar wael <omarwael33@gmail.com>
2023-07-25 10:46:51 +0200

    adding mappings from link to comment

--- src/main/java/com/example/reddit/domain/Comment.java
+++ src/main/java/com/example/reddit/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 @Entity
 @NoArgsConstructor
@@ -15,6 +16,9 @@ public class Comment {
     @GeneratedValue
     private  long id ;
     private String body;
+
+    @ManyToOne
+    private Link link;
 
 
 
--- src/main/java/com/example/reddit/domain/Link.java
+++ src/main/java/com/example/reddit/domain/Link.java
@@ -1,12 +1,16 @@
 package com.example.reddit.domain;
 
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.OneToMany;
 import javax.persistence.Id;
-import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
+
 
 @Entity
 @NoArgsConstructor
@@ -18,6 +22,10 @@ public class Link {
 
     private String title;
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
+
 
 
 
