7ef8cc35ae44bc27b306f779febab4a0ff60202d
Dawood <omarwael33@gmail.com>
2023-07-27 19:01:33 +0200

    error with login in user

---
+++ src/main/java/com/example/reddit/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.reddit.repository;
+
+import com.example.reddit.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
---
+++ src/main/java/com/example/reddit/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.reddit.security;
+
+import com.example.reddit.domain.User;
+import com.example.reddit.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+
+        if(!user.isPresent()){
+            throw new UsernameNotFoundException(username);
+        }else{
+            return user.get();
+        }
+
+    }
+}
